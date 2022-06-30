e1f9cef78b06c571f59b32d5101d28df0040164b
SrapOK <viktor2469ck@gmail.com>
2022-06-30 19:12:56 +0300

    normis

--- Creature.h
+++ Creature.h
@@ -16,29 +16,34 @@ extern Map mapee;
 class Creature
 {
 protected:
+	bool up_is_pressed;
+	void collision_x();
+	void collision_y();
 	std::string _name;
-	int _x, _y;
 	float _dx, _dy;
 	float _speed;
+	sf::FloatRect box;
 
 	int _width, _height;
 	sf::Sprite _sprite;
 	std::map<std::string, Animation*> animations;
+	bool on_ground;
 
 public:
 	Creature(int __x, int __y, int __width, int __height);
 	void add_animation(Animation*);
-	int x(void) const;
-	int y(void) const;
+	float& x(void);
+	float& y(void);
 	float dx(void) const;
 	float dy(void) const;
-	int width(void) const;
-	int height(void) const;
+	float& width(void);
+	float& height(void);
 	const sf::Sprite& sprite(void) const;
 	void set_default_sprite(std::string __path);
 	void set_default_sprite(Animation& __Anim_part);
 	void set_default_sprite(sf::Texture& __Texture_part);
 	void get_command(float);
+	void move(void);
 //	int grtVector2f();
 	//virtual void update(float) = 0;
 };
